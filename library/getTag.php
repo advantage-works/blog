488b893c25ceb693e7dea7501d5638a44e214a4a
advantage-works <advantage@advantage-works.net>
2023-07-16 10:21:56 +0900

    webフォント適用

--- library/getTag.php
+++ library/getTag.php
@@ -11,6 +11,9 @@ function getHead(string $title): void
     <link rel="stylesheet" href="https://advantage-works.net/blogtest/reset.css">
     <link rel="stylesheet" href="https://advantage-works.net/blogtest/style.css">
     <link rel="stylesheet" href="https://advantage-works.net/blogtest/style600.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     </head>';
 }
 
